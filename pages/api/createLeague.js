f2dee6dda33046760ab601bd57373b880877c39e
Nicola Rinaldi <rinaldi.nicola@outlook.it>
2022-08-27 22:27:34 +0200

    added auth to createLeague, TODO: middleware auth

--- pages/api/createLeague.js
+++ pages/api/createLeague.js
@@ -1,7 +1,24 @@
 import { prisma } from "../../prisma/client";
+import jwt from "jsonwebtoken";
 
 const handler = async (req, res) => {
-  const { leagueName, username } = req.body;
+  const authHeader = req.headers["authorization"];
+  console.log(authHeader);
+  const token = authHeader && authHeader.split(" ")[1];
+  let username;
+  if (!token) {
+    res.status(401).json({ message: "token not provided" });
+    return;
+  } else {
+    try {
+      const payload = jwt.verify(token, process.env.JWT_KEY);
+      username = payload.username;
+    } catch (error) {
+      res.status(403).json({ message: "Token not valid or not provided" });
+      return;
+    }
+  }
+  const { leagueName } = req.body;
 
   const existingLeague = await prisma.League.findFirst({
     where: { name: leagueName },
@@ -10,6 +27,7 @@ const handler = async (req, res) => {
     // 400 error status codes are for client errors, this is not! It's just a coincidence
     // it's common to use a 200 OK with a flag in the body (ok in this case) setted to false
     res.status(200).json({ ok: false, message: "league name already taken" });
+    return;
   }
 
   //get admin data (I need the id as foreign key)
